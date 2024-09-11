9337d01506746b07b9fb7987df0900794692d9e3
Анатолий <malahtolya@gmail.com>
2024-09-11 20:35:45 +0600

    fix

--- CryptoLibrary/PublicKey.cs
+++ CryptoLibrary/PublicKey.cs
@@ -7,66 +7,42 @@ using System.Text;
 
 namespace Crypto;
 
-public static class KeyEncryption
-{
-#pragma warning disable CA2211
-    public static Algorithm algo = Algorithm.AES;
-#pragma warning restore CA2211
-    public enum Algorithm
-    {
-        AES,
-        Elgamal
-    }
-}
 public static class PublicKey
 {
     public static string ReceiveSessionKey (NetworkStream stream, Helper helper)
     {
         StringBuilder logs = new();
-        if (KeyEncryption.algo == KeyEncryption.Algorithm.AES)
-        {
-            byte [] encryptedSessionKey = new byte [256];
-            stream.Read(encryptedSessionKey);
-            byte [] sessionKey = helper.rsa.Decrypt(encryptedSessionKey, false);
-            helper.aes.Key = sessionKey;
-            byte [] iv = new byte [helper.aes.BlockSize / 8];
-            stream.Read(iv);
-            helper.aes.IV = iv;
+        byte [] encryptedSessionKey = new byte [256];
+        stream.Read(encryptedSessionKey);
+        byte [] sessionKey = helper.rsa.Decrypt(encryptedSessionKey, false);
+        helper.aes.Key = sessionKey;
+        byte [] iv = new byte [helper.aes.BlockSize / 8];
+        stream.Read(iv);
+        helper.aes.IV = iv;
 
-            logs.AppendLine($"Зашифрованный cеансовый ключ получен: {BitConverter.ToString(encryptedSessionKey)}");
-            logs.AppendLine(Environment.NewLine + $"Сеансовый ключ расшифрован: {BitConverter.ToString(sessionKey)}");
-            return logs.ToString();
-        }
-        else
-        {
-            throw new ArgumentException("Unknown Key encryption algorithm");
-        }
+        logs.AppendLine($"Зашифрованный cеансовый ключ получен: {BitConverter.ToString(encryptedSessionKey)}");
+        logs.AppendLine(Environment.NewLine + $"Сеансовый ключ расшифрован: {BitConverter.ToString(sessionKey)}");
+        return logs.ToString();
     }
 
     public static string SendSessionKey (NetworkStream stream, Helper helper, string publicKey)
     {
         StringBuilder logs = new();
-        if (KeyEncryption.algo == KeyEncryption.Algorithm.AES)
-        {
-            helper.rsa.FromXmlString(publicKey);
-            helper.aes.GenerateKey();
-            helper.aes.GenerateIV();
+        helper.rsa.FromXmlString(publicKey);
+        helper.aes.GenerateKey();
+        helper.aes.GenerateIV();
 
-            byte [] sessionKey = helper.aes.Key;
-            byte [] iv = helper.aes.IV;
+        byte [] sessionKey = helper.aes.Key;
+        byte [] iv = helper.aes.IV;
 
-            byte [] encryptedSessionKey = helper.rsa.Encrypt(sessionKey, false);
-            stream.Write(encryptedSessionKey);
+        byte [] encryptedSessionKey = helper.rsa.Encrypt(sessionKey, false);
+        stream.Write(encryptedSessionKey);
 
-            logs.AppendLine($"Сеансовый ключ создан: {BitConverter.ToString(sessionKey)}");
-            logs.AppendLine(Environment.NewLine + $"Сеансовый ключ зашифрован и отправлен: {BitConverter.ToString(encryptedSessionKey)}");
-            stream.Write(iv);
-            return logs.ToString();
-        }
-        else
-        {
-            throw new ArgumentException("Unknown Key encryption algorithm");
-        }
+        logs.AppendLine($"Сеансовый ключ создан: {BitConverter.ToString(sessionKey)}");
+        logs.AppendLine(Environment.NewLine + $"Сеансовый ключ зашифрован и отправлен: {BitConverter.ToString(encryptedSessionKey)}");
+        stream.Write(iv);
+        return logs.ToString();
+
     }
 
     public static void SendPublicKey (NetworkStream stream, string publicKey)
